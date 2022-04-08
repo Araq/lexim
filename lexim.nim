ab17dd9869cea021b2028c621faac1987ee6aae9
Araq <rumpf_a@web.de>
2022-04-08 11:01:39 +0200

    lexim.nim: supports -d:leximSkipLexe

--- lexim.nim
+++ lexim.nim
@@ -10,6 +10,8 @@
 import
   regexprs, nfa, macros, marshal
 
+proc findMacro(name: string): PRegExpr {.used.} = nil
+
 proc newRange(a, b: NimNode): NimNode {.compileTime.} =
   newCall(bindSym"..", a, b)
 
@@ -33,14 +35,21 @@ proc charSetLit(cc: set[char]): NimNode {.compileTime.} =
     if c1 >= MaxChar: break
     inc c1
 
-proc currChar(s, i: NimNode): NimNode {.compileTime.} =
-  newTree(nnkBracketExpr, s, i)
+proc charAt(s: string; i: int): char {.inline.} =
+  result = if i < s.len: s[i] else: '\0'
 
-proc getCmp(s, i: NimNode; x: set[char]): NimNode {.compileTime.} =
-  result = newCall(bindSym"contains",  charSetLit(x), currChar(s, i))
+proc currChar(s, i: NimNode; isCString: bool): NimNode {.compileTime.} =
+  result =
+    if isCString:
+      newTree(nnkBracketExpr, s, i)
+    else:
+      newCall(bindSym"charAt", s, i)
 
-proc getSpecial(s, i: NimNode; x: Alphabet): NimNode {.compileTime.} =
-  result = newCall(bindSym"==", currChar(s, i), newLit(x.val))
+proc getCmp(s, i: NimNode; x: set[char]; isCString: bool): NimNode {.compileTime.} =
+  result = newCall(bindSym"contains",  charSetLit(x), currChar(s, i, isCString))
+
+proc getSpecial(s, i: NimNode; x: Alphabet; isCString: bool): NimNode {.compileTime.} =
+  result = newCall(bindSym"==", currChar(s, i, isCString), newLit(x.val))
 
 proc newVarStmt(name, typ, value: NimNode): NimNode {.compiletime.} =
   return newTree(nnkVarSection, newTree(nnkIdentDefs, name, typ, value))
@@ -48,7 +57,7 @@ proc newVarStmt(name, typ, value: NimNode): NimNode {.compiletime.} =
 proc nextState(i, state: NimNode; dest: int): NimNode {.compileTime.} =
   newStmtList(newCall(bindSym"inc", i), newAssignment(state, newLit(dest)))
 
-proc genMatcher(a: DFA; s, i, bodies: NimNode): NimNode {.compileTime.} =
+proc genMatcher(a: DFA; s, i, bodies: NimNode; isCString: bool): NimNode {.compileTime.} =
   let state = genSym(nskVar, "state")
   result = newStmtList()
   result.add newVarStmt(newTree(nnkPragmaExpr, state,
@@ -66,12 +75,12 @@ proc genMatcher(a: DFA; s, i, bodies: NimNode): NimNode {.compileTime.} =
       let (others, cs) = allTransitions(a, src, dest)
       if cs != {}:
         ifStmt.add newTree(nnkElifBranch,
-                           getCmp(s, i, cs),
+                           getCmp(s, i, cs, isCString),
                            nextState(i, state, dest))
       for ot in others:
         if ot.kind == reChar:
           ifStmt.add newTree(nnkElifBranch,
-                             getSpecial(s, i, ot),
+                             getSpecial(s, i, ot, isCString),
                              nextState(i, state, dest))
         else:
           doAssert false, "not supported " & $ot.kind
@@ -90,23 +99,50 @@ template `/.`(x: string): string =
   (when defined(posix): "./" & x else: x)
 
 macro match*(s: cstring|string; pos: int; sections: varargs[untyped]): untyped =
-  var res: seq[string] = @[]
-  for sec in sections.children:
-    expectKind sec, nnkOfBranch
-    expectLen sec, 2
-    if sec[0].kind in nnkStrLit..nnkTripleStrLit:
-      res.add sec[0].strVal
-    else:
-      error("Expected a node of kind nnkStrLit, got " & $sec[0].kind)
+  let isCString = s.getType.typeKind == ntyCString
+  when defined(leximSkipLexe):
+    var bigRe: PRegExpr = nil
+    var rule = 1
+    for sec in sections.children:
+      expectKind sec, nnkOfBranch
+      expectLen sec, 2
+      if sec[0].kind in nnkStrLit..nnkTripleStrLit:
+        let rex = parseRegExpr(sec[0].strVal, findMacro,
+                              {reNoCaptures, reNoBackrefs})
+        rex.rule = rule
+        if bigRe.isNil: bigRe = rex
+        else: bigRe = altExpr(bigRe, rex)
+      else:
+        error("Expected a node of kind nnkStrLit, got " & $sec[0].kind)
+      inc rule
 
-  let data = $$res
-  writeFile("lexe.input", data)
-  let o = to[DFA](staticExec(/."lexe", input="", cache=data))
-  result = genMatcher(o, s, pos, sections)
-  #echo repr result
+    var n: NFA
+    var d, o: DFA
+    regExprToNFA(bigRe, n)
+    let alph = fullAlphabet(n)
+    NFA_to_DFA(n, d, alph)
+    optimizeDFA(d, o, alph)
+    result = genMatcher(o, s, pos, sections, isCString)
+  else:
+    # use 'lexe.exe' helper program in order to speedup lexer generation
+    var res: seq[string] = @[]
+    for sec in sections.children:
+      expectKind sec, nnkOfBranch
+      expectLen sec, 2
+      if sec[0].kind in nnkStrLit..nnkTripleStrLit:
+        res.add sec[0].strVal
+      else:
+        error("Expected a node of kind nnkStrLit, got " & $sec[0].kind)
+
+    let data = $$res
+    writeFile("lexe.input", data)
+    let o = to[DFA](staticExec(/."lexe", input="", cache=data))
+    result = genMatcher(o, s, pos, sections, isCString)
+  echo repr result
 
 when isMainModule: # defined(testing):
   var input = "the 0909 else input elif elseo end"
+  let asc = input.cstring
   var pos = 0
   while pos < input.len:
     let oldPos = pos
